727a16bdb2094bd72061ec7e9376bbd62fb4f853
mobao_ios <mobao_ios@zzdeiMac.local>
2015-07-28 10:18:14 +0800

    Initial Commit

---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by mobao_ios on 15/7/28.
+//  Copyright (c) 2015年 mobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by mobao_ios on 15/7/28.
+//  Copyright (c) 2015年 mobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
